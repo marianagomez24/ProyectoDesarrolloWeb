61f2e93f060a85e4be018b74f9c789d93c3d9336
Mariana Gomez <95109233+marianagomez24@users.noreply.github.com>
2022-06-09 12:08:24 -0600

    Add files via upload
    Registro y Inicio de sesión

---
+++ TareaPaginasProyecto/src/main/java/com/Tarea/TareaPaginas/PaginaProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea.TareaPaginas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaProyectoApplication.class, args);
+	}
+
+}
---
+++ TareaPaginasProyecto/src/test/java/com/Tarea/TareaPaginas/PaginaProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea.TareaPaginas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
